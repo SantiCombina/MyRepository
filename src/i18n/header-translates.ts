fc6b897dd4b3de7a567ecf8c705535a24827ed42
SantiCombina <santicombina@gmail.com>
2024-03-13 00:56:26 -0300

    Updated resume EN

--- src/i18n/header-translates.ts
+++ src/i18n/header-translates.ts
@@ -3,12 +3,12 @@ export const headerTranslate = {
         title: "Hi, I'm",
         bio: "I develop 3D websites, user interfaces and web applications",
         resume_download: "Resume",
-        resume_link: "",
+        resume_link: "https://drive.google.com/file/d/1KWvOunA7hk48JcWJklDdI7CucydcAq9N/view?usp=sharing",
     },
     ES: {
         title: "Hola, soy",
         bio: "Desarrollo sitios 3D, interfaces de usuario y aplicaciones web",
         resume_download: "Currículum",
-        resume_link: "https://drive.google.com/file/d/1ERQrFAIlqnNeLUnpIElvYUXQH9E1QPjz/view?usp=sharing",
+        resume_link: "https://drive.google.com/file/d/1-TkvyHrniFImT1LPldWuAuqDI8RxKb5d/view?usp=sharing",
     },
 };
