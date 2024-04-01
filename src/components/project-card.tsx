91c4f2817b44fb434d0fcc9cd385e58840500040
SantiCombina <santicombina@gmail.com>
2024-04-01 19:25:20 -0300

    Made project card bigger

--- src/components/project-card.tsx
+++ src/components/project-card.tsx
@@ -17,7 +17,7 @@ export function ProjectCard({image, name, description, deploy, repository}: Prop
         "px-3 text-[#DFD9FF] transition-transform duration-300 bg-[#100d25] rounded-md active:scale-100 shadow-primary shadow-md hover:scale-110";
 
     return (
-        <article className="rounded-xl max-w-[320px] bg-[#100D25] p-2 flex flex-col justify-between gap-2 hover:bg-[#151030]">
+        <article className="rounded-xl max-w-[350px] bg-[#100D25] p-2 flex flex-col justify-between gap-2 hover:bg-[#151030]">
             <div className="flex flex-col">
                 <img alt="project image" className="rounded-t-lg select-none" src={image} />
                 <h2 className="px-3 pt-2 font-bold">{name}</h2>
