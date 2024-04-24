ee09d26231576670685dc0ce3f60249b34b30a34
SantiCombina <santicombina@gmail.com>
2024-04-24 19:13:17 -0300

    Changed selection color

--- src/views/portfolio.tsx
+++ src/views/portfolio.tsx
@@ -13,7 +13,7 @@ export function Portfolio() {
     const contactRef = useRef(null);
 
     return (
-        <div className="min-h-screen overflow-y-auto bg-no-repeat bg-primary bg-hero-pattern scroll-smooth font-monserrat">
+        <div className="min-h-screen overflow-y-auto bg-no-repeat bg-primary bg-hero-pattern scroll-smooth font-monserrat selection:bg-fuchsia-600">
             <Navbar contactRef={contactRef} headerRef={headerRef} projectsRef={projectsRef} skillsRef={skillsRef} />
             <Header headerRef={headerRef} />
             <Projects projectsRef={projectsRef} />
