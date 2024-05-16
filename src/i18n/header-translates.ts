20d244019036362a7bc17cc66f3fda6e9733d077
SantiCombina <santicombina@gmail.com>
2024-05-15 21:15:06 -0300

    Update CV

--- src/i18n/header-translates.ts
+++ src/i18n/header-translates.ts
@@ -3,12 +3,14 @@ export const headerTranslate = {
         title: "Hi, I'm",
         bio: "I develop landing pages, user interfaces and web applications",
         resume_download: "Resume",
-        resume_link: "https://drive.google.com/file/d/1KWvOunA7hk48JcWJklDdI7CucydcAq9N/view?usp=sharing",
+        resume_link:
+            "https://o365sanfranciscoutneduar-my.sharepoint.com/:b:/g/personal/scombina_facultad_sanfrancisco_utn_edu_ar/ERI0MjlrTgJGk15jsbA5hW8BPi_rsVBCK4ZFyfA1p_2_jg?e=72Pmim",
     },
     ES: {
         title: "Hola, soy",
         bio: "Desarrollo landing pages, interfaces de usuario y aplicaciones web",
         resume_download: "Currículum",
-        resume_link: "https://drive.google.com/file/d/1-TkvyHrniFImT1LPldWuAuqDI8RxKb5d/view?usp=sharing",
+        resume_link:
+            "https://o365sanfranciscoutneduar-my.sharepoint.com/:b:/g/personal/scombina_facultad_sanfrancisco_utn_edu_ar/EW8FPVcfeu5Cq4iePIy9OkgBkwDEmV_3JGFun1SIDznnTw?e=Sdk6n3",
     },
 };
